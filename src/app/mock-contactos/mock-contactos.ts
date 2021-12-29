1dac562150c73df8f863d1a0a506da62763191da
Maria Infantes Lacal <minfantesl@ext.vectoritcgroup.com>
2021-12-29 12:58:17 +0100

    integrados breackpoints

--- src/app/mock-contactos/mock-contactos.ts
+++ src/app/mock-contactos/mock-contactos.ts
@@ -2,7 +2,7 @@
 import Contacto from "../model/Contacto";
 
 export const CONTACTOS: Contacto[] = [
-    {nombreContacto:"Pepe", telefono:"623475869", fechaNacimiento:"17/03/1977", sexo:"hombre", cervezasFavoritas:[{id:1,name:'buzz',description:'asasdas',image_url:'ajskhdioo.png'},{id:1,name:'buzz',description:'asasdas',image_url:'ajskhdioo.png'}]},
+    {nombreContacto:"Pepe", telefono:"623475869", fechaNacimiento:"17/03/1977", sexo:"hombre", cervezasFavoritas:[{id:1,name:'buzz',description:'asasdas',image_url:'ajskhdioo.png'},{id:1,name:'buzz',description:'asasdas',image_url:'ajskhdioo.png'},{id:1,name:'buzz',description:'asasdas',image_url:'ajskhdioo.png'}]},
     {nombreContacto:"Pepe", telefono:"623475869", fechaNacimiento:"17/03/1977", sexo:"hombre", cervezasFavoritas:[{id:1,name:'buzz',description:'asasdas',image_url:'ajskhdioo.png'}]},
     {nombreContacto:"Pepe", telefono:"623475869", fechaNacimiento:"17/03/1977", sexo:"hombre", cervezasFavoritas:[{id:1,name:'buzz',description:'asasdas',image_url:'ajskhdioo.png'}]},
     {nombreContacto:"Pepe", telefono:"623475869", fechaNacimiento:"17/03/1977", sexo:"hombre", cervezasFavoritas:[{id:1,name:'buzz',description:'asasdas',image_url:'ajskhdioo.png'}]},
