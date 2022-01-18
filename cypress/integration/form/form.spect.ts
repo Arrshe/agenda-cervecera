f513f71604488cab211d344f8034c78a3b4904e5
Maria Infantes Lacal <minfantesl@ext.vectoritcgroup.com>
2022-01-18 13:56:05 +0100

    feat: creados test de rellenar formulario y enviar y de rellenar formulario y limpiarlo

--- cypress/integration/form/form.spect.ts
+++ cypress/integration/form/form.spect.ts
@@ -1,5 +1,7 @@
+import { Input } from "@angular/core";
+
 describe("formulario de registro", () => {
-    it("abre la página de registro y rellena el formulario", () => {
+    it("abre la página de registro, rellena el formulario y lo envía", () => {
         cy.visit("/");
 
         //nombre input
@@ -9,13 +11,53 @@ describe("formulario de registro", () => {
         cy.get('#telefono').type('628930475');
 
         //fecha de nacimiento
-
+        cy.get('#fechaNacimiento').click().type('1997-12-04');
 
         //sexo
-
+        cy.get("#sexoItem").check()
 
         //cervezas favoritas
+        cy.get(".agenda-cervecera-alta__checkbox--wrapper")
+            .children('#0').check()
+        cy.get(".agenda-cervecera-alta__checkbox--wrapper")
+            .children('#3').check()
+        cy.get(".agenda-cervecera-alta__checkbox--wrapper")
+            .children('#5').check()
 
-        
+
+        //submit
+        cy.get(".agenda-cervecera-alta__submit").click()
     });
+
+});
+
+describe("formulario de registro", () => {
+    it("abre la página de registro, rellena el formulario y lo limpia", () => {
+        cy.visit("/");
+
+        //nombre input
+        cy.get('#nombreContacto').type('pepe');
+
+        //telefono input
+        cy.get('#telefono').type('628930475');
+
+        //fecha de nacimiento
+        cy.get('#fechaNacimiento').click().type('1997-12-04');
+
+        //sexo
+        cy.get("#sexoItem").check()
+
+        //cervezas favoritas
+        cy.get(".agenda-cervecera-alta__checkbox--wrapper")
+            .children('#0').check()
+        cy.get(".agenda-cervecera-alta__checkbox--wrapper")
+            .children('#3').check()
+        cy.get(".agenda-cervecera-alta__checkbox--wrapper")
+            .children('#5').check()
+
+
+        //submit
+        cy.get(".agenda-cervecera-alta__clear").click()
+    });
+
 });
