8d0769bbe7ccc66cb89b90f29950e1d3513e86d3
Maria Infantes Lacal <minfantesl@ext.vectoritcgroup.com>
2022-02-01 12:14:32 +0100

    feat: cucumber preprocessor added

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -1,3 +1,7 @@
 // Plugins enable you to tap into, modify, or extend the internal behavior of Cypress
 // For more info, visit https://on.cypress.io/plugins-api
-module.exports = (on: any, config: any) => {}
+const cucumber = require('cypress-cucumber-preprocessor').default;
+
+module.exports = (on: any, config: any) => {
+  on('file:preprocessor', cucumber());
+};
